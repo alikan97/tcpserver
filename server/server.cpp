92b509af36b54ad9debbe4c37e7fc33fc2ef5d39
alikan97 <alikhanhassan2@gmail.com>
2022-04-20 23:11:56 +1000

    updates

--- server/server.cpp
+++ server/server.cpp
@@ -11,9 +11,19 @@
 
 using namespace std;
 
-int main() {
-    int listening = socket(AF_INET,SOCK_STREAM,0);
-    if(listening == -1) {
+// TODO
+// Create central queue
+// Multithreading / multiClient
+// Resource allocation / memory mgmt
+// Sending structs in message
+
+int main()
+{
+    int PORT = 5000;
+
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
         cerr << "Can't create socket" << endl;
         return -1;
     }
@@ -21,13 +31,13 @@ int main() {
     // Bind the socket to an IP
     sockaddr_in hint;
     hint.sin_family = AF_INET;
-    hint.sin_port = htons(5000);
-    inet_pton(AF_INET, "0.0.0.0",&hint.sin_addr);
+    hint.sin_port = htons(PORT);
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
-    if (bind(listening,(sockaddr*) &hint, sizeof(hint)) == -1)
+    if (bind(listening, (sockaddr *)&hint, sizeof(hint)) == -1)
     {
         cerr << "Can't bind to IP/Port" << endl;
-	    return -2;
+        return -2;
     }
 
     // Mark the scoket for listening
@@ -37,44 +47,74 @@ int main() {
         return -3;
     }
 
-    // Accept a caller
-    sockaddr_in client;
-    socklen_t clientSize;
-    char host[NI_MAXHOST];
-    char svc[NI_MAXSERV];
+    cout << "Server has started listening on Port: " << PORT << endl;
+    signal(SIGINT, interrupt_handler); // Handle System Terminations gracefully
 
-    clientSize = sizeof(client);
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-    if (clientSocket == -1)
+    while (1)
     {
-        cerr << "Problem with client connecting";
-        return -4;
-    }
+        // Accept a caller
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+        char host[NI_MAXHOST];
+        char svc[NI_MAXSERV];
 
-    close(listening);
-    memset(host, 0, NI_MAXHOST);
-    memset(svc, 0, NI_MAXSERV);
+        int clientSocket = accept(listening, (sockaddr *)&client, &clientSize);
+        if (clientSocket == -1)
+        {
+            cerr << "Problem with client connecting";
+            return -4;
+        }
 
-    int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-    if (result)
-    {
-	    cout << host << " connected on " << svc << endl;
-    } else {
-	    inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-	    cout << host << " connected on " << ntohs(client.sin_port) << endl;
-    }
-
-    char buff[100];
-
-    while (1) {
-	memset(buff, 0, 100);
-	cout << "Enter message to send" << endl;
-	cin >> buff;
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        send(clientSocket, buff,50, 0);
+        std::thread th1(handleConnection, clientSocket, client, &host, &svc);
     }
 
     close(clientSocket);
     return 0;
+}
+
+void interrupt_handler(int clientSocket)
+{
+    std::cout << "Recevied Intertupt: Shutting Down!..." << endl;
+    sleep(1);
+    close(clientSocket);
+    exit(0);
+}
+
+void handleConnection(int clientSocket, sockaddr_in client, char *host, char *svc)
+{
+    memset(host, 0, NI_MAXHOST);
+    memset(svc, 0, NI_MAXSERV);
+
+    int result = getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+    if (result)
+    {
+        cout << host << " connected on " << svc << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on " << ntohs(client.sin_port) << endl;
+    }
+    char buff[4096];
+    while (1)
+    {
+        memset(buff, 0, 4096);
+        int bytesRecv = recv(clientSocket, buff, 4096,0);
+        if (bytesRecv == -1)
+        {
+            std::cout << "Connection issue" << std::endl;
+            break;
+        }
+        if (bytesRecv == 0)
+        {
+            std::cout << "Client disconnected" << std::endl;
+            break;
+        }
+
+        cout << "Received " << string(buff, 0, bytesRecv) << endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        send(clientSocket, buff, bytesRecv-1, 0);
+    }
+    close(clientSocket);
 }
--- server/test.cpp
+++ server/test.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <queue>
 #include <cstring>
+#include <thread> 
+#include <mutex>
+
+std::mutex mtx;
 
 typedef struct message_t
 {
@@ -9,13 +13,31 @@ typedef struct message_t
     std::string datetime;
 }* message;
 
+void func (std::queue<message_t> *mq)
+{
+    mtx.lock();
+    if (!mq->empty()) {
+        std::cout << "Messages in the queue " << mq->front().message << std::endl;
+    } else {
+        message_t myMessage = { "First Message", "20220/1/03"};
+        mq->push(myMessage);
+    }
+    mtx.unlock();
+}
+
 int main (int argc, char** argv)
 {
-    std::queue<message> myQueue;
-    message frs = new message_t;
-    frs->message = "First message";
-    frs->datetime = "2022/04/18";
-    myQueue.push(frs);
+    int maxSize = 100;
+    std::queue<message_t> myQueue;
 
-    std::cout << myQueue.back()->message;
+    // std::strncpy(buff, first, (size_t)strlen(first)+1);   // strcpy will overwrite what was previouslyt in the buffer, +1 will allocate one more room for null termination
+    // std::strcat(buff, second);
+
+    std::thread th1(func, &myQueue);
+    std::thread th2(func, &myQueue);
+    std::thread th3(func, &myQueue);
+
+    th1.join();
+    th2.join();
+    th3.join();
 }
