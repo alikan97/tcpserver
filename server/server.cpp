492ce065d7eabaf080be1b5ad61bca8ab98336c2
ali.hassan <ali.hassan@blackbook.ai>
2022-04-24 22:01:50 +1000

    more tinz

--- server/server.cpp
+++ server/server.cpp
@@ -11,16 +11,18 @@
 #include <signal.h>
 #include <mutex>
 #include <queue>
+#include "List.h"
+#include <algorithm>
 
 using namespace std;
 
 std::mutex globalMtx;
 
 // TODO
-// Create central queue
+// Create central data store
 // Multithreading / multiClient
 // Resource allocation / memory mgmt
-// Sending structs in message
+// Source data from sensors
 
 void interrupt_handler(int clientSocket)
 {
@@ -30,13 +32,12 @@ void interrupt_handler(int clientSocket)
     exit(0);
 }
 
-void handleConnection(int clientSocket, sockaddr_in client, char *host, char *svc, std::queue<std::string> *mq);
+void handleConnection(int clientSocket, sockaddr_in client, char *host, char *svc);
 
 int main()
 {
-    int PORT = 5000;
-    std::queue<std::string> messageQueue;
-
+    int PORT = 5005;
+    
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
@@ -75,14 +76,11 @@ int main()
         char svc[NI_MAXSERV];
 
         int clientSocket;
-	int connectedClients = 0;
 
         while ((clientSocket = accept(listening, (sockaddr *)&client, &clientSize)) != -1)
         {
-	    connectedClients++;
-	    std::cout << "Client accepted, Total number of connected clients: " << connectedClients << std::endl;
-	    std::thread th1(handleConnection, clientSocket, client, std::ref(host), std::ref(svc), &messageQueue);
-	    th1.detach();
+            std::thread th1(handleConnection, clientSocket, client, std::ref(host), std::ref(svc));
+            th1.detach();
         }
 
         close(clientSocket);
@@ -90,9 +88,22 @@ int main()
     return 0;
 }
 
-void handleConnection(int clientSocket, sockaddr_in client, char *host, char *svc, std::queue<std::string> *mq)
+void handleConnection(int clientSocket, sockaddr_in client, char *host, char *svc)
 {
     std::cout << "New thread initiated..." << std::endl;
+    int values[10] = {4,2,55,3,2,12,6,45,6,3};
+    std::queue<std::string> messageQueue;
+
+    messageQueue.push("First message");
+    messageQueue.push("Second Message");
+    messageQueue.push("Third message");
+    messageQueue.push("Fourth Message");
+    messageQueue.push("Fifth message");
+    messageQueue.push("Sixth Message");
+
+    char newClient[26] = "Welcome to the server!";
+    send(clientSocket, newClient, 21, 0);
+
     memset(host, 0, NI_MAXHOST);
     memset(svc, 0, NI_MAXSERV);
 
@@ -106,38 +117,15 @@ void handleConnection(int clientSocket, sockaddr_in client, char *host, char *sv
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
         cout << host << " connected on " << ntohs(client.sin_port) << endl;
     }
-    char buff[4096];
-    while (1)
+
+    while (!messageQueue.empty())
     {
-	globalMtx.lock();
-	if (!mq->empty())
-	{
-		std::cout << "MessageQueue: " << mq->front() << std::endl;
-		mq->pop();
-	}
-	globalMtx.unlock();
-
-        memset(buff, 0, 4096);
-        int bytesRecv = recv(clientSocket, buff, 4096, 0);
-        if (bytesRecv == -1)
-        {
-            std::cout << "Connection issue" << std::endl;
-            break;
-        }
-        if (bytesRecv == 0)
-        {
-            std::cout << "Client disconnected" << std::endl;
-            break;
-        }
-
-	std::string receivedString = string(buff, 0, bytesRecv);
-
-        cout << "Received " << receivedString << endl;
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-	globalMtx.lock();
-        mq->push(receivedString);
-	globalMtx.unlock();
+        std::cout << messageQueue.size() << endl;
+        int bytes = send(clientSocket, messageQueue.front().c_str(), messageQueue.front().size(), 0);
+        messageQueue.pop();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
     close(clientSocket);
 }
+
+
